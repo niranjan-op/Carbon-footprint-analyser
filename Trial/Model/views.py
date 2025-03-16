eea0653ddb702a72172383f2de63ba6ef99e0305
Niranjan Gadgil <niranjanvgce123@gst.sies.edu.in>
2025-03-17 00:18:08 +0530

    Minor Changes
    1. Completed the frontend for Configure_Constants

--- Trial/Model/views.py
+++ Trial/Model/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
 def Calculator(request):
     return render(request,'Calculator/Calculator.html')
+
 def Configure_Constants(request):
     return render(request, 'Configure_Constants/Configure_Constants.html')
 
