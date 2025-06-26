a8e9ee0eb859e57ecc6e4f23345e84f33982a67b
Niranjan Gadgil <niranjanvgce123@gst.sies.edu.in>
2025-06-26 13:35:40 +0530

    Update settings.py

--- Trial/settings.py
+++ Trial/settings.py
@@ -79,15 +79,16 @@ WSGI_APPLICATION = 'Trial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_URL = 'postgresql://minefootprint_user:reXqi5AO7sLeMacooXd8o2bFmO77nnvs@dpg-d1edff2li9vc739u964g-a/minefootprint'
+# Remove this hardcoded DATABASE_URL (major security risk)
+# DATABASE_URL = 'postgresql://minefootprint_user:reXqi5AO7sLeMacooXd8o2bFmO77nnvs@dpg-d1edff2li9vc739u964g-a/minefootprint'
+
+# Database configuration - CORRECTED VERSION
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'default':dj_database_url.config(
-        default= os.environ.get('DATABASE_URL')
-        )
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
